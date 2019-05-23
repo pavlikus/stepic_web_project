95216be72d8d0e70643335a7d41a5b6c7a683f90
pavlik <readystores.ru@gmail.com>
2019-05-23 18:08:32 +0300

    Edit init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+echo 'Create links, remove default config from /etc/nginx/sites-enabled/default'
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default 2> /dev/null
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-﻿sudo /etc/init.d/mysql start﻿
+sudo /etc/init.d/mysql start
+echo 'Done'
+exit
